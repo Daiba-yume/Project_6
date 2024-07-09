f4088f4e82274df189c9a64068de40895635fd21
daiba <daiba_b@yahoo.com>
2024-07-09 22:40:44 +0200

    Correction accessibilité

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -20,7 +20,7 @@ function photographerTemplate(data) {
     // Création de l'élément photo profil
     const img = document.createElement("img");
     img.setAttribute("src", picture);
-    img.setAttribute("alt", name);
+    img.setAttribute("alt", "");
 
     // Création de l'élément, affiche le nom
     const h2 = document.createElement("h2");
